bf12e4377046b7a8f6a9d32172b0324456145158
xungdeptrysieucapvippro <xunglord@linux6313.h2cloud.vn>
2026-03-13 06:06:09 +0700

    upgrade enviroments

--- frontend/enviroments/environment-prod.ts
+++ frontend/enviroments/environment-prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  product: false,
+  product: true,
   apiBaseUrl: '/api',
-  fileBaseUrl: 'http://localhost:8080',
+  fileBaseUrl: 'https://xunglord.com',
   googleClientId: '777794922407-knmfkv6c29521061f2gccmfn0cvbt8j4.apps.googleusercontent.com'
 };
